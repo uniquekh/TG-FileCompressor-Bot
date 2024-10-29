b82edccb5e058e41a301da2b2f81d83155683ff0
Unique <mujeem000@gmail.com>
2024-10-29 07:38:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from sample_config import Config
+
+class Development(Config):
+  APP_ID = 21179966
+  API_HASH = "d97919fb0a3c725e8bb2a25bbb37d57c"
+  TG_BOT_TOKEN = "7568715797:AAHaLrNo9syTvyKzqitEvEZKSUw_tILUMpk"
+  AUTH_USERS = [
+    7326397503
+  ]
